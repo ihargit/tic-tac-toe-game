0cc2decd8e299b7d31b11e8af9a44374bdccedd8
ihargit <ihar.kharashun@gmail.com>
2019-08-25 21:25:17 +0300

    Updates

---
+++ precache-manifest.4232a80c09f122c5346652bcc6b58443.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0135222c8a9dd8a9386c7c8262dce15b",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "4b9e9873762f7ff93f91",
+    "url": "/tic-tac-toe-game/static/css/main.eb5502a1.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe-game/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "4b9e9873762f7ff93f91",
+    "url": "/tic-tac-toe-game/static/js/main.a33b8ddc.chunk.js"
+  },
+  {
+    "revision": "05fb09dae5993cefe3f3",
+    "url": "/tic-tac-toe-game/static/js/runtime~main.28992ff7.js"
+  }
+]);
